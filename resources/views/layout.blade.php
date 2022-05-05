7b5fe2ec6b04052d8e04e9025d0839fd664af94b
pickle99 <jackrestler@gmail.com>
2022-05-06 00:00:37 +0400

    style: markup styling in layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,18 +2,18 @@
 
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
-<body>
+<body class="bg-gray-500">
 <div class="w-full flex justify-center pt-56">
-    <div>
-
+    <div class="max-w-sm">
         @foreach($movies as $movie)
-            <img class="w-60"  src="{{asset('storage/'.$movie->name.'.jpg')}}" alt="img" />
-                    <p class="pt-4 font-bold"> Movie: {{ $movie->name }}</p>
-        <p>Quote: {{$movie->quotes[random_int(0,2)]->name}}</p>
-
-
+            <div class="items-center">
+            <img class="max-w-sm"  src="{{asset('storage/'.$movie->name.'.jpg')}}" alt="img" />
+              <div class="text-center">
+                    <p class="pt-2 font-bold"> Movie: {{ $movie->name }}</p>
+        <p class="pt-2.5">Quote: {{$movie->quotes[random_int(0,2)]->name}}</p>
+              </div>
+            </div>
         @endforeach
-
     </div>
 </div>
 </body>
