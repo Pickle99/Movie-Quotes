82724a6601c31fe69975d1172069a0bafc989790
pickle99 <jackrestler@gmail.com>
2022-05-08 00:05:37 +0400

    fix: image on home and quotes pages fixed

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -19,18 +19,6 @@ class CrudController extends Controller
 
 	public function store()
 	{
-//		$attributes = request()->validate([
-//			'name'  => 'required',
-//			'image' => 'required',
-//		]);
-//		$quoteatt = request()->validate([
-//			'name'     => 'required',
-//			'movie_id' => $movie->id,
-//		]);
-//		$attributes['image'] = request()->file('image')->store('images');
-//		Movie::create($attributes);
-//		Quote::create($quoteatt);
-//		return redirect('/');
 		$validated = request()->validate([
 			'name'  => 'required',
 			'image' => 'required',
@@ -39,9 +27,9 @@ class CrudController extends Controller
 		$validator = request()->validate([
 			'quote' => 'required',
 		]);
-
+		$validated['image'] = request()->file('image')->store('images');
 		$movie = Movie::create($validated);
-//		$movie->image->create(['image' => $validated['image']]);
+
 		Quote::create([
 			'movie_id' => $movie->id,
 			'quote'    => $validator['quote'],
